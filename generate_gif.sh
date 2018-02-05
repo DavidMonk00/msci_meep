35053920da4d39b25d107d7d8f5d152ebe57cebc
David Monk <d.g.monk0@gmail.com>
2018-02-05 22:22:12 +0000

    Pre merge commit

--- generate_gif.sh
+++ generate_gif.sh
@@ -10,6 +10,6 @@ do
   time h5topng -t 0:$frames -R -Zc dkbluered -a yarg -A $PROG-eps-000000.00.h5 $i
   echo "Converting to gif..."
   rm test.mp4
-  time ffmpeg -r 10 -f image2 -i $PROG-ez.t%03d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p test.mp4
+  time ffmpeg -r 30 -f image2 -i $PROG-ez.t%03d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p test.mp4
 done
 echo "Done"
