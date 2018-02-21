7358703cfd8e3c3b48500b43e05ba0f61d909639
David Monk <d.g.monk0@gmail.com>
2018-02-21 17:11:09 +0000

    Changed run file

--- guile-MEEP/cavity_resonator/run.sh
+++ guile-MEEP/cavity_resonator/run.sh
@@ -1,9 +1,5 @@
 rm -rf cavity_resonator-out/*.png
-<<<<<<< HEAD
-meep cavity_resonator.ctl #&> out.log
-=======
 meep cavity_resonator_sweep.ctl | awk '/harminv/ {print $2, $4}'
 meep cavity_resonator.ctl &> out.log
 python rename_images.py
 cat ./cavity_resonator-out/*.png | ffmpeg -y -f image2pipe -i - output.mp4
->>>>>>> cb7e996a55e996fdd5b8c63ca842de0e2af91251
