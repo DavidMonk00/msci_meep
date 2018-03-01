8569dbec883acd10f63056ce99af9248a2bafee9
David Monk <d.g.monk0@gmail.com>
2018-03-01 11:05:33 +0000

    Increased resolution

--- guile-MEEP/stripline/run.sh
+++ guile-MEEP/stripline/run.sh
@@ -1,20 +1,41 @@
 
 NPROC=$(nproc)
+echo "Running MEEP..."
 mpirun -np $((NPROC/2)) meep-openmpi stripline.ctl # &> out.log
+
 cd stripline-out
+
+# Create y slice video
+echo "Creating y-slice..."
 read frames <<<$( h5ls ez.h5 | awk '{split($6,a,"/");print a[1]}' )
 read y_len <<<$( h5ls ez.h5 | awk '{split($4,a,",");print a[1]}' )
-y_len=$(($y_len - 1))
-y_len=$(($y_len*4/5))
-frames=$(($frames-1))
-h5topng -y $y_len -t 0:$frames -R -Zc dkbluered -a yarg ez.h5 -m -0.0025 -M 0.0025
-rm -rf img
-mkdir img
-mv *.png ./img/
-for i in ./img/*.png; do
+y_len=$((($y_len-1)*4/5))
+h5topng -y $y_len -t 0:$((frames-1)) -R -Zc dkbluered -a yarg ez.h5 # -m -0.02 -M 0.02
+rm -rf img/y-slice
+mkdir -p img/y-slice
+mv *.png ./img/y-slice/
+for i in ./img/y-slice/*.png; do
   read x <<<$( identify $i | awk '{split($3,a,"x");print a[1]}' )
   read y <<<$( identify $i | awk '{split($3,a,"x");print a[2]}' )
   convert $i -crop $((x-4))x$((y-4))+2+2 $i
 done
 python ../rename_images.py
-cat ./img/*.png | ffmpeg -y -f image2pipe -i - output.mkv # &> /dev/null
+cat ./img/y-slice/*.png | ffmpeg -y -f image2pipe -i - output_y_slice.mkv &> /dev/null
+
+# Create z-slice at source
+echo "Creating z-slice..."
+read frames <<<$( h5ls ez.h5 | awk '{split($6,a,"/");print a[1]}' )
+read z_len <<<$( h5ls ez.h5 | awk '{split($5,a,",");print a[1]}' )
+z_len=$((($z_len-1)*7/10))
+h5topng -z $z_len -t 0:$((frames-1)) -R -Zc dkbluered -a yarg ez.h5 # -m -0.002 -M 0.002
+rm -rf img/z-slice
+mkdir -p img/z-slice
+mv *.png ./img/z-slice/
+for i in ./img/z-slice/*.png; do
+  read x <<<$( identify $i | awk '{split($3,a,"x");print a[1]}' )
+  read y <<<$( identify $i | awk '{split($3,a,"x");print a[2]}' )
+  convert $i -crop $((x-4))x$((y-4))+2+2 $i
+done
+python ../rename_images.py
+cat ./img/z-slice/*.png | ffmpeg -y -f image2pipe -i - output_z_slice.mkv &> /dev/null
+echo "Done."
--- guile-MEEP/stripline/stripline.ctl
+++ guile-MEEP/stripline/stripline.ctl
@@ -1,43 +1,43 @@
 (define-param w 10)
-(define-param l 20)
+(define-param l 40)
 (define-param h 10)
 (define-param sx w)
 (define-param sy l)
 (define-param sz h)
 (define-param stripH 0.5)
 (define-param stripW 2)
-(define-param dpml 0.1)
-(define-param res 15)
+(define-param dpml 0.5)
+(define-param res 16)
 (define-param freq 0.1)
 
 (set-param! resolution res)
 (set! eps-averaging? false)
 (set! k-point (vector3 0 0 0))
-(set! ensure-periodicity true)
+; (set! ensure-periodicity true)
 
 (set! geometry-lattice (make lattice (size sx sy sz)))
 (define geom (list
   ; Substrate dielectric
   (make block
     (center 0 0 (/ h 4))
-    (size w l (/ h 2))
+    (size infinity infinity (/ h 2))
     (material (make dielectric (epsilon 12)))
   )
   ; centre strip
   (make block
     (center 0 0 (/ stripH 2))
-    (size stripW l stripH)
+    (size stripW infinity stripH)
     (material metal)
   )
   ; ground plates
   (make block
     (center (/ (- w stripW) 2) 0 (/ stripH 2))
-    (size stripW l stripH)
+    (size stripW infinity stripH)
     (material metal)
   )
   (make block
     (center (/ (- w stripW) -2) 0 (/ stripH 2))
-    (size stripW l stripH)
+    (size stripW infinity stripH)
     (material metal)
   )
 ))
@@ -50,6 +50,7 @@
 
 (use-output-directory)
 
+; source
 (set! sources (list
   (make source
     (src (make continuous-src
@@ -58,8 +59,8 @@
       (end-time (/ 10 freq))
     ))
     (component Ez)
-    (center 0 (- (/ l 5) (/ l 2)) 2)
-    (size 0 0)
+    (center 0 (- (/ l 6) (/ l 2)) 3)
+    (size (/ 0.5 freq) 0.5 0.5)
   )
 ))
 
