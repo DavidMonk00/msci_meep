935397438977986655af6799581c160f26e383c4
David Monk <d.g.monk0@gmail.com>
2018-03-03 15:25:30 +0000

    Created new folder

---
+++ guile-MEEP/stripline-current/run.sh
@@ -0,0 +1,52 @@
+NPROC=$(nproc)
+
+echo "Setting variables..."
+if [[ $# -eq 0 ]] ; then
+    echo 'ERROR: No resolution given, please enter resolution for simulation as argument.'
+    exit 0
+fi
+sed -i "s/param res .*)/param res $1)/g" ./stripline.ctl
+
+echo "Running MEEP..."
+mpirun -np $((NPROC/2)) meep-openmpi stripline.ctl # &> out.log
+
+cd stripline-out
+
+# Create y slice video
+echo "Creating y-slice..."
+read frames <<<$( h5ls ez.h5 | awk '{split($6,a,"/");print a[1]}' )
+read y_len <<<$( h5ls ez.h5 | awk '{split($4,a,",");print a[1]}' )
+y_len=$((($y_len-1)*4/5))
+h5topng -y $y_len -t 0:$((frames-1)) -R -Zc dkbluered -a yarg ez.h5 # -m -0.02 -M 0.02
+rm -rf img/y-slice
+mkdir -p img/y-slice
+mv *.png ./img/y-slice/
+for i in ./img/y-slice/*.png; do
+  read x <<<$( identify $i | awk '{split($3,a,"x");print a[1]}' )
+  read y <<<$( identify $i | awk '{split($3,a,"x");print a[2]}' )
+  convert $i -crop $((x-1))x$((y-1))+1+1 $i
+done
+python ../rename_images.py
+cat ./img/y-slice/*.png | ffmpeg -y -f image2pipe -i - output_y_slice.mkv &> /dev/null
+
+# Create z-slice at source
+echo "Creating z-slice..."
+read frames <<<$( h5ls ez.h5 | awk '{split($6,a,"/");print a[1]}' )
+read z_len <<<$( h5ls ez.h5 | awk '{split($5,a,",");print a[1]}' )
+z_len=$((($z_len-1)*7/10))
+h5topng -z $z_len -t 0:$((frames-1)) -R -Zc dkbluered -a yarg ez.h5 # -m -0.002 -M 0.002
+rm -rf img/z-slice
+mkdir -p img/z-slice
+mv *.png ./img/z-slice/
+for i in ./img/z-slice/*.png; do
+  read x <<<$( identify $i | awk '{split($3,a,"x");print a[1]}' )
+  read y <<<$( identify $i | awk '{split($3,a,"x");print a[2]}' )
+  convert $i -crop $((x-1))x$((y-1))+1+1 $i
+done
+python ../rename_images.py
+cat ./img/z-slice/*.png | ffmpeg -y -f image2pipe -i - output_z_slice.mkv &> /dev/null
+echo "Done."
+
+cd ..
+
+python analysis.py
---
+++ guile-MEEP/stripline-current/stripline-current.ctl
@@ -0,0 +1,71 @@
+(define-param Xdim 20)
+(define-param Ydim 60)
+(define-param Zdim 10)
+(define-param sx Xdim)
+(define-param sy Ydim)
+(define-param sz Zdim)
+(define-param t 0.5)      ; Strip thickness
+(define-param S 2)        ; Thickness of centre strip
+(define-param W 2)        ; Width of gap
+(define-param dpml 0.5)
+(define-param res 5)
+(define-param freq 0.1)
+
+(set-param! resolution res)
+(set! eps-averaging? false)
+(set! k-point (vector3 0 0 0))
+; (set! ensure-periodicity true)
+
+(set! geometry-lattice (make lattice (size sx sy sz)))
+(define geom (list
+  ; Substrate dielectric
+  (make block
+    (center 0 0 (/ Zdim -4))
+    (size infinity infinity (/ Zdim 2))
+    (material (make dielectric (epsilon 12)))
+  )
+  ; centre strip
+  (make block
+    (center 0 0 (/ t 2))
+    (size S infinity t)
+    (material metal)
+  )
+  ; ground plates
+  (make block
+    (center (/ (+ Xdim (+ (* 2 W) S)) -4) 0 (/ t 2))
+    (size (/ (- Xdim (+ (* 2 W) S)) 2) infinity t)
+    (material metal)
+  )
+  (make block
+    (center (/ (+ Xdim (+ (* 2 W) S)) 4) 0 (/ t 2))
+    (size (/ (- Xdim (+ (* 2 W) S)) 2) infinity t)
+    (material metal)
+  )
+))
+
+(set! geometry geom)
+
+(set! pml-layers (list (make pml
+  (thickness dpml)
+)))
+
+(use-output-directory)
+
+; source
+(set! sources (list
+  (make source
+    (src (make continuous-src
+      (frequency freq)
+      (width 20)
+      (end-time (/ 40 freq))
+    ))
+    (component Ez)
+    (center 0 (- (/ Ydim 6) (/ Ydim 2)) 3)
+    (size S 0 0)
+  )
+))
+
+(run-until (/ 40 freq)
+  (at-beginning output-epsilon)
+  (to-appended "ez" (at-every 1 output-efield-z))
+)
